3e4ec96bbfc6935f476ac93eddc4382e65a95739
Dirk <dirk@mintvm.(none)>
2012-02-06 20:30:08 +0100

    Fixed case of includes, updated headers

--- upnp/upnpclient.cpp
+++ upnp/upnpclient.cpp
@@ -1,14 +1,22 @@
+//    Copyright (C) 2012 Dirk Vanden Boer <dirk.vdb@gmail.com>
 //
-//  upnpclient.cpp
-//  UPnPFramework
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
 //
-//  Created by Dirk Vanden Boer on 06/06/11.
-//  Copyright 2011 None. All rights reserved.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
 //
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "upnpclient.h"
 
-#include "Utils/stringoperations.h"
+#include "utils/stringoperations.h"
 
 namespace UPnP
 {
