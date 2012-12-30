03045856b5528bf85da27e8cdf6e3bc304a8b27f
Dirk Vanden Boer <dirk.vdb@gmail.com>
2012-12-30 12:39:20 +0100

    Dlna type checking

---
+++ inc/upnp/upnpdlnainfo.cpp
@@ -0,0 +1,9 @@
+//
+//  upnpdlnainfo.cpp
+//  UPnPFramework
+//
+//  Created by Dirk Vanden Boer on 10/11/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#include "upnpdlnainfo.h"
---
+++ inc/upnp/upnpdlnainfo.h
@@ -0,0 +1,14 @@
+//
+//  upnpdlnainfo.h
+//  UPnPFramework
+//
+//  Created by Dirk Vanden Boer on 10/11/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#ifndef __UPnPFramework__upnpdlnainfo__
+#define __UPnPFramework__upnpdlnainfo__
+
+#include <iostream>
+
+#endif /* defined(__UPnPFramework__upnpdlnainfo__) */
