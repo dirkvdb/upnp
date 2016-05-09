23c559e00a38f4952e38aced1d67500fd69522b2
Dirk Vanden Boer <dirk.vdb@gmail.com>
2016-05-09 21:29:00 +0200

    pragma once

--- inc/upnp/upnpserviceclientbase.h
+++ inc/upnp/upnpserviceclientbase.h
@@ -14,8 +14,7 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-#ifndef UPNP_SERVICE_H
-#define UPNP_SERVICE_H
+#pragma once
 
 #include "utils/log.h"
 #include "utils/signal.h"
@@ -266,5 +265,3 @@ private:
 };
 
 }
-
-#endif
