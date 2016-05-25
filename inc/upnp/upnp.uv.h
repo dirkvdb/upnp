0b14f606838f2fc7307dfc410656f2bade3c2d98
Dirk Vanden Boer <dirk.vdb@gmail.com>
2016-05-25 21:57:11 +0200

    Remove unused apply functions

--- inc/upnp/upnp.uv.h
+++ inc/upnp/upnp.uv.h
@@ -432,32 +432,6 @@ private:
     std::function<void()> m_callback;
 };
 
-template<size_t N>
-struct Apply
-{
-    template<typename F, typename T, typename... A>
-    static inline auto apply(F&& f, T&& t, A&&... a)
-    {
-        return Apply<N-1>::apply(std::forward<F>(f), std::forward<T>(t), std::get<N-1>(std::forward<T>(t)), std::forward<A>(a)...);
-    }
-};
-
-template<>
-struct Apply<0>
-{
-    template<typename F, typename T, typename... A>
-    static inline auto apply(F&& f, T&&, A&&... a)
-    {
-        return std::forward<F>(f)(std::forward<A>(a)...);
-    }
-};
-
-template<typename F, typename T>
-inline auto apply(F && f, T && t)
-{
-    return Apply<std::tuple_size<std::decay_t<T>>::value>::apply(std::forward<F>(f), std::forward<T>(t));
-}
-
 template <typename ...Ts>
 class Async : public Handle<uv_async_t>
 {
