1f06e427734bd685e17bf91696241edd059d433e
Dirk Vanden Boer <dirk.vdb@gmail.com>
2017-08-16 00:01:02 +0200

    Fixed head method

--- src/upnp.http.client.cpp
+++ src/upnp.http.client.cpp
@@ -165,7 +165,7 @@ void Client::performRequest(std::function<void(const std::error_code&)> cb)
 void Client::receiveData(std::function<void(const std::error_code&)> cb)
 {
     m_timer.expires_from_now(m_timeout);
-
+    m_response.body.reserve(10 * 1024 * 1024);
     beast::http::async_read(m_socket, m_buffer, m_response, [this, cb] (const asio_error_code& error) {
         m_timer.cancel();
         if (invokeCallbackOnError(error, cb))
@@ -197,6 +197,7 @@ void Client::receiveHeaderData(std::function<void(const std::error_code&)> cb)
     m_timer.expires_from_now(m_timeout);
 
     auto parser = std::make_shared<beast::http::parser<false, beast::http::empty_body>>();
+    parser->skip(true); // tell the parser not to expect a body
     beast::http::async_read_header(m_socket, m_buffer, *parser, [this, cb, parser] (const asio_error_code& error) {
         m_timer.cancel();
         if (invokeCallbackOnError(error, cb))
